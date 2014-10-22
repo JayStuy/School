804265e6ce3a9b5c5f085d6c08dc7da6c3a93ac1
Jay <jfromentrudder@gmail.com>
2014-10-22 09:47:19 -0400

    Update Character.java

--- Character.java
+++ Character.java
@@ -1,14 +1,14 @@
 public class Character{
-//Instance Variables
-int HP;
-String name;
+  //Instance Variables
+  int HP; 
+  String name;
 
-public Character(){
-  name = "DW";
-  HP = 15;
-}
-public Character(String n){
-  name = n;
-  HP = 80;
-}
+  public Character(){
+    name = "DW";
+    HP = 15;
+  }
+  public Character(String n){
+    name = n;
+    HP = 80;
+  }
 }
