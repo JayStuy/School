4aab1839b471ab601a89a31c8598b0e11c6929f7
cdelano266 <kim.thunderbird@gmail.com>
2014-10-21 21:38:20 -0400

    Update Character.java

--- Character.java
+++ Character.java
@@ -1,5 +1,14 @@
 public class Character{
 //Instance Variables
 int HP;
+String name;
 
+public Character(){
+  name = "DW";
+  HP = 15;
+}
+public Character(String n){
+  name = n;
+  HP = 80;
+}
 }
